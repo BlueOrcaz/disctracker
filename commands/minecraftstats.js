2f944e55a2745ccf39a10bd25c70f2d60fb135f1
Bond <bondoldman@gmail.com>
2022-05-28 20:27:49 +1000

    MCCommit

---
+++ commands/minecraftstats.js
@@ -0,0 +1,46 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { fs } = require('fs');
+
+
+module.exports = {
+
+    name: "mcskin",
+    category: "accounts",
+    description: "accounts",
+    usage: "mcskin <playername>",
+    
+	data: new SlashCommandBuilder()
+		.setName('minecraftstats')
+		.setDescription('replies with your discord stats')
+        .addStringOption(option => 
+            option
+                .setName('mcuser')
+                .setDescription('Put Minecraft: Java Edition username in here to check user stats')
+                .setRequired(false) 
+            ),
+                
+        
+	async execute(interaction) { 
+    
+    const user = interaction.options.get("mcuser").value;
+    //   const user = interaction.options.getString("string", false);
+    var mcapi = require("mcapi");
+    let uuid = await mcapi.usernameToUUID(user);
+    
+    const { MessageEmbed } = require('discord.js');
+    const Embed = new MessageEmbed()
+        .setColor('RANDOM')
+        .setTitle('Minecraft User Statistics')
+        .setAuthor({ name: 'DiscTracker#5743', iconURL: 'https://i.imgur.com/063Nm4O.png' /*, url: 'https://discord.js.org' */ })
+        .setDescription(`Minecraft user statistics for ${user}`)
+        .setThumbnail(`https://minotar.net/bust/${user}/100.png`)
+        .addFields(
+            { name: 'Username:', value: `${user}`},
+            { name: 'UUID:', value: `${uuid}`},
+            { name: 'Skin:', value: `[Download](https://minotar.net/download/${user})`},
+            { name: 'More Info:', value:`[NameMC](https://mine.ly/${user}.1)`},
+        )
+        .setTimestamp()
+        await interaction.reply({ embeds: [Embed] });
+	},
+};
