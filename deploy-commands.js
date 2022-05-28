ce980de444c80f423888f823ffc5f32c9d561e18
Bond <bondoldman@gmail.com>
2022-05-28 17:12:24 +1000

    updated config

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,7 +1,7 @@
 const fs = require('node:fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId, token, guildId} = require('./config.json');
+const { clientId, token } = require('./config.json');
 
 
 const commands = [];
