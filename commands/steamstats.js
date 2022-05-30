91233f7dbb025ccbf6f41d26da5245c1a00e89b1
U87PM <iliaglvb07@gmail.com>
2022-05-30 18:32:51 +1000

    Update steamstats.js

--- commands/steamstats.js
+++ commands/steamstats.js
@@ -51,6 +51,29 @@ module.exports = {
             console.log(bans)
             console.log(groups)
             console.log(ownedgames)
+
+            const { MessageEmbed } = require('discord.js');
+    
+           const Embed  = new MessageEmbed()
+               .setColor('RANDOM')
+               .setTitle('Steam User Statistics')
+               .setAuthor({ name: 'DiscTracker#5743', iconURL: 'https://i.imgur.com/063Nm4O.png' /*, url: 'https://discord.js.org' */ })
+               .setDescription(`Steam user statistics for ${summary.nickname}`)
+               .setThumbnail(summary.avatar.large)
+               .addFields(
+                   { name: 'Nickname:', value: summary.nickname },
+                   { name: 'Account Creation Date:', value: `<t:${Math.round(summary.createdAt / 1000)}:R>` },
+                   { name: 'Account Level:', value: `${lvl}`},
+                   { name: 'Number of Games Owned:', value: `${JSON.stringify(ownedgames.length)}`},
+                   { name: 'Last Played:', value: `${JSON.stringify(recentgames[0].name)}`},
+                   { name: 'Number of VAC Bans:', value: `${JSON.stringify(bans.vacBans)}`},
+                   { name: 'Days since last VAC Ban:', value: `${JSON.stringify(bans.daysSinceLastBan)}`},
+                   { name: 'Last Online:', value: `<t:${Math.round(summary.lastLogOffAt / 1000)}:R>`},
+                   { name: 'Profile Link:', value: summary.url}
+                   
+               )
+               interaction.reply({ embeds: [Embed]});
+               return id, summary, lvl, recentgames, bans, groups, ownedgames;
         }
         messageSend();
     }, 
