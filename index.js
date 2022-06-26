bf995bd155bb4a1c77a10c3a1e056c238a154490
Bond Tran <43434743+BlueOrcaz@users.noreply.github.com>
2022-06-26 16:16:33 +1000

    CatchError

--- index.js
+++ index.js
@@ -50,7 +50,13 @@ client.on('interactionCreate', async interaction => {
 		await command.execute(interaction); // executes command
 	} catch (error) {
 		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true }); // logs an error 
+		const { MessageEmbed } = require('discord.js');
+		const Embed = new MessageEmbed()
+		.setColor("RANDOM")
+		.setTitle("Error")
+		.setDescription("There was an Error exceuting this Command! Please make sure that your account details are set to public and/or you didn't mistype!")
+		.setTimestamp()
+		await interaction.reply({ embeds: [Embed] });
 	}
 });
 
@@ -62,12 +68,7 @@ client.on('shardError', error => {
 process.on('unhandledRejection', error => {
 	console.error('Unhandled promise rejection:', error);
 
-	const { MessageEmbed } = require('discord.js');
-	const Embed = new MessageEmbed()
-		.setColor("RANDOM")
-		.setTitle("Test")
-		.setTimestamp()
-	interaction.reply({ embeds: [Embed] });
+	
 
 });
 
