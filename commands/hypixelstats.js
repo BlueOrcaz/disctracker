d87b7404a4aa8cb24568768e7627f08b98d0d27a
Bond Tran <43434743+BlueOrcaz@users.noreply.github.com>
2022-07-19 18:17:35 +1000

    WIP Schemas

--- commands/hypixelstats.js
+++ commands/hypixelstats.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const axios = require('axios').default;
+const mongoose = require('mongoose');
 
 
 
@@ -17,18 +18,22 @@ module.exports = {
             ),
     
 	async execute(interaction) {  
+        const HUser = require('../schemas/HypixelSchema');
 
+
+        
 
         const user = interaction.options.get("username").value;
         const key = process.env.HYPIXEL_KEY;
         axios.get(`https://api.hypixel.net/player?key=${key}&name=${user}`)
+            
             .then(function (stats) {
                 const { MessageEmbed } = require('discord.js');
                 const Embed = new MessageEmbed()
                 .setColor("RANDOM")
                     .setTitle("Hypixel User Statistics")
                     .setAuthor({ name: 'DiscTracker#5743', iconURL: 'https://i.imgur.com/063Nm4O.png' /*, url: 'https://discord.js.org' */ })
-                    .setDescription(`Hypixel User Statistics for ${stats.data.player.displayname}`)
+                    // .setDescription(`Hypixel User Statistics for ${stats.data.player.displayname}`)
                     .setThumbnail(`https://pbs.twimg.com/profile_images/1346968969849171970/DdNypQdN_400x400.png`)
                     .addFields(
                         { name: 'Display Name:', value: `${stats.data.player.displayname}` },
@@ -36,16 +41,21 @@ module.exports = {
                         { name: 'Last Online:', value: `<t:${Math.round(stats.data.player.lastLogin / 1000)}:R>`},
                         { name: 'Previous Usernames', value: `${stats.data.player.knownAliases.join(', ')}` },
                         { name: 'Number of Achievements', value: `${stats.data.player.achievementsOneTime.length}` },
-                        { name: 'UUID', value: `${stats.data.player.uuid}` }
+                        { name: 'UUID', value: `${stats.data.player.uuid}` } 
                     )
                 .setTimestamp()
-
-                
                 interaction.reply({ embeds: [Embed]});
+
+
+                // const savedUser = newUser.save();
+
             })  
             .catch(function (error) {
                 console.log(error);
             });
+
+          
+            
 	},
 };
 
